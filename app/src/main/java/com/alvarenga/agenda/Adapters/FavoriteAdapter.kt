5b04d50dde099598359512f0921ac41bbd2c1b2e
Rodrigo Alvarenga <00017316@uca.edu.sv>
2018-05-03 14:46:49 -0500

    Added favorite adapter for Favoriteview fragment. Extends from RecyclerView

---
+++ app/src/main/java/com/alvarenga/agenda/Adapters/FavoriteAdapter.kt
@@ -0,0 +1,90 @@
+package com.alvarenga.agenda.Adapters
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.alvarenga.agenda.Contacts.Contact
+import com.alvarenga.agenda.Fragments.ContactFragment
+import com.alvarenga.agenda.Fragments.FavoriteFragment
+import com.alvarenga.agenda.R
+import de.hdodenhof.circleimageview.CircleImageView
+
+class FavoriteAdapter(private val contact: ArrayList<Contact>, private val context:Context, private val fav:Boolean): RecyclerView.Adapter<FavoriteAdapter.FavoriteViewHolder>() {
+
+    companion object {
+        @SuppressLint("StaticFieldLeak")
+        private val contFrag = ContactFragment.getInstance()
+        @SuppressLint("StaticFieldLeak")
+        private val favFrag = FavoriteFragment.getInstance()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteViewHolder {
+        /*if(tabInstance!!.getItem(0) == Contacts){
+            val v = LayoutInflater.from(parent.context).inflate(R.layout.activity_contactview,parent,false)
+            return FavoriteViewHolder(v, this.fav)
+        }else{
+            val v = LayoutInflater.from(parent.context).inflate(R.layout.activity_favoriteview,parent,false)
+            return FavoriteViewHolder(v, this.fav)
+        }*/
+        /* if(tabInstance!!.getPageTitle(tabLayout.selectedTabPosition)!!.equals(0)){
+             val v = LayoutInflater.from(parent.context).inflate(R.layout.activity_contactview,parent,false)
+             return FavoriteViewHolder(v, this.fav)
+         }else{
+             val v = LayoutInflater.from(parent.context).inflate(R.layout.activity_favoriteview,parent,false)
+             return FavoriteViewHolder(v, this.fav)
+         }*/
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.activity_favoriteview,parent,false)
+        return FavoriteViewHolder(v, this.fav)
+    }
+
+    override fun getItemCount(): Int {
+        return contact.size
+    }
+
+    override fun onBindViewHolder(holder: FavoriteViewHolder, position: Int) {
+        holder.name.text = contact[position].name
+        holder.img.setImageResource(contact[position].photo)
+        /*holder.favButton.setImageResource(if(contact[position].isFav)
+            R.drawable.star_full else R.drawable.star_empty)
+        holder.favButton.setOnClickListener { v ->
+            Toast.makeText(v.context, contact[position].name, Toast.LENGTH_SHORT).show()
+            if(!fav){
+                if(!holder.fav){
+                    contact[position].isFav = true
+                    holder.favButton.setImageResource(R.drawable.star_full)
+                    holder.fav = true
+                } else{
+                    contact[position].isFav = false
+                    holder.favButton.setImageResource(R.drawable.star_empty)
+                    holder.fav = false
+                }
+            }else{
+                if (!holder.fav) {
+                    contact[position].isFav = true
+                    holder.favButton.setImageResource(R.drawable.star_full)
+                    holder.fav = true
+                } else {
+                    contact[position].isFav = false
+                    holder.favButton.setImageResource(R.drawable.star_empty)
+                    holder.fav = false
+                }
+                contFrag.update(contact, position, holder.fav)
+            }
+            favFrag.update(contact)
+        }*/
+    }
+
+    class FavoriteViewHolder(itemView: View, internal var fav:Boolean):RecyclerView.ViewHolder(itemView) {
+        /*internal var cardView:CardView = itemView.findViewById(R.id.card_view)*/
+        internal var name: TextView = itemView.findViewById(R.id.name)
+        internal var img:CircleImageView = itemView.findViewById(R.id.img)/*
+        internal var favButton: ImageView = itemView.findViewById(R.id.butt)*/
+    }
+
+}
