4ea451aa93ac534eccf974d13d63e009f7b71970
Rodrigo Alvarenga <00017316@uca.edu.sv>
2018-05-03 13:41:36 -0500

    Added support for Toolbar on actionbar. Created 3 Contact instances for the moment, for testing. Created adapter of ViewPagerAdapter tpy and render it to viewPager section

---
+++ app/src/main/java/com/alvarenga/agenda/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.alvarenga.agenda
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v4.view.ViewPager
+import android.support.v7.widget.Toolbar
+import com.alvarenga.agenda.Adapters.ViewPagerAdapter
+import com.alvarenga.agenda.Contacts.Contact
+
+class MainActivity : AppCompatActivity() {
+
+    var contacts:ArrayList<Contact>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val myToolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(myToolbar)
+        contacts = ArrayList<Contact>().apply {
+            add(Contact(R.drawable.n_letter,"Nelson Castro", 72936840, "nelsonmandela@gmail.com","DESC",false))
+            add(Contact(R.drawable.m_letter,"Mancha ", 72936840, "nelsonmandela@gmail.com","DESC",false))
+            add(Contact(R.drawable.l_letter,"Lito", 72936840, "nelsonmandela@gmail.com","DESC",false))
+        }
+
+        val bundle = Bundle()
+        bundle.putParcelableArrayList("KEY", contacts)
+
+        val tab = findViewById<TabLayout>(R.id.mainTab)
+        val viewer = findViewById<ViewPager>(R.id.Pager)
+        val adapter = ViewPagerAdapter(this,supportFragmentManager,bundle)
+
+        viewer.adapter = adapter
+        tab.setupWithViewPager(viewer)
+    }
+}
