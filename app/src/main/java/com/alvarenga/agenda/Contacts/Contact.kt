9e3ea4dd02ad4a055ed6aab28dc7fb9aace2e7ce
Rodrigo Alvarenga <00017316@uca.edu.sv>
2018-05-03 14:49:51 -0500

    Created Contact class(object) wich implements from parcelable

---
+++ app/src/main/java/com/alvarenga/agenda/Contacts/Contact.kt
@@ -0,0 +1,40 @@
+package com.alvarenga.agenda.Contacts
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Contact(var photo: Int, var name:String, var phone:Int,var email:String,var other:String, var isFav: Boolean):Parcelable{
+
+    //Generated code from parcelable implementation
+    constructor(parcel: Parcel) : this(
+            parcel.readInt(),
+            parcel.readString(),
+            parcel.readInt(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readByte() != 0.toByte())
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(photo)
+        parcel.writeString(name)
+        parcel.writeInt(phone)
+        parcel.writeString(email)
+        parcel.writeString(other)
+        parcel.writeByte(if (isFav) 1 else 0)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Contact> {
+        override fun createFromParcel(parcel: Parcel): Contact {
+            return Contact(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Contact?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
