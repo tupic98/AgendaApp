43e8cf78f80e06254c7e1653192f200af9d6cbb3
Rodrigo Alvarenga <00017316@uca.edu.sv>
2018-05-06 00:01:20 -0500

    Created ContactInfo activity for contact info show

---
+++ app/src/main/java/com/alvarenga/agenda/ContactinfoActivity.kt
@@ -0,0 +1,11 @@
+package com.alvarenga.agenda
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ContactinfoActivity:AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_view)
+    }
+}
